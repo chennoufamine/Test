da8a2d30ee6052e8b70c82e0c9bffcc5dc445abb
Amine.Chennouf <Amine.Chennouf@DT-IT-02.alsalamalgeria.local>
2022-01-26 10:17:32 +0100

    First Commit

---
+++ src/test/TestClass.java
@@ -0,0 +1,19 @@
+package test;
+
+/**
+ * TODO: Document me!
+ *
+ * @author Amine.Chennouf
+ *
+ */
+public class TestClass {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Test");
+    }
+
+}
